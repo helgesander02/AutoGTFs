0cf931b4185e8fe9733faf83d5e97cac78df864c
Sander <helgesan0202@gmail.com>
2024-11-26 08:34:23 +0000

    update cypress task for download image

--- cypress.config.cjs
+++ cypress.config.cjs
@@ -1,8 +1,43 @@
+const fs = require('fs');
+const path = require('path');
+const axios = require('axios');
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
+      on('task', {
+        async downloadImageFromUrl({ count, format }) {
+          const imageUrls = [];
+          for (let i = 0; i < count; i++) {
+            // API: https://picsum.photos/
+            imageUrls.push(`https://picsum.photos/200/300.jpg?random=${i + 1}`);
+          }
+
+          const imagePaths = [];
+          const folderPath = path.join(__dirname, 'cypress', 'downloads');
+          for (let i = 0; i < imageUrls.length; i++) {
+            const url = imageUrls[i];
+            const fileName = `download_${count}_photo${i + 1}.${format}`;
+            const filePath = path.join(folderPath, fileName);
+
+            try {
+              const response = await axios({
+                url,
+                method: 'GET',
+                responseType: 'arraybuffer',
+              });
+              await fs.promises.writeFile(filePath, response.data);
+              imagePaths.push(filePath);
+
+            } catch (error) {
+              throw new Error(`Error downloading image from ${url}: ${error.message}`);
+
+            }
+          }
+          return imagePaths;
+        },
+      });
     },
   },
 });
